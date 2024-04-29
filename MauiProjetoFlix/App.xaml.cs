32fa18e35976710a810793eb2159f668e47a507f
Usuario <Usuario@CPD03-M03>
2024-04-29 15:35:40 -0300

    Adicionar arquivos de projeto.

---
+++ MauiProjetoFlix/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiProjetoFlix
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiProjetoFlix/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiProjetoFlix
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
